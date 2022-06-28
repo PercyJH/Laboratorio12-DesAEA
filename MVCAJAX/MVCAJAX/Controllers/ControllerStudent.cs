9efa866b13c55ceca27db71079f10b387b01eef0
Percy Herrera Valdez <percy_j98@hotmail.com>
2022-06-27 23:44:51 -0500

    work done

---
+++ MVCAJAX/MVCAJAX/Controllers/ControllerStudent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using DOMAIN;
+using SERVICE;
+using MVCAJAX.Models;
+
+
+namespace MVCAJAX.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private StudentService service = new StudentService();
+
+        // GET: Students
+        public ActionResult IndexRazor()
+        {
+            var model = (from c in service.Get()
+                         select new StudentModel
+                         {
+                             studentID = c.studentID,
+                             studentName = c.studentName,
+                             studentLastName = c.studentLastName,
+                             studentAddress = c.studentAddress,
+                             studentCode = c.studentCode
+                         }).ToList();
+            return View(model);
+        }
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getStudent(string id)
+        {
+            return Json(service.Get(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult createStudent(Student std)
+        {
+            service.Insert(std);
+            string message = "SUCCESS";
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+    }
+}
---
+++ MVCAJAX/MVCAJAX/Models/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAJAX.Models
+{
+    public class StudentModel
+    {
+        public int studentID { get; set; }
+        public string studentName { get; set; }
+        public string studentAddress { get; set; }
+        public string studentLastName { get; set; }
+        public string studentCode { get; set; }
+    }
+}
